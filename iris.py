a639447b9fcbba27eb28aa04b86c4c16c143c9d4
Akshat Agarwal <akag@users.noreply.github.com>
2018-10-06 22:54:43 +0530

    data set loading and visualisation

--- iris.py
+++ iris.py
@@ -5,3 +5,50 @@ Created on Sat Oct  6 22:41:37 2018
 @author: Akshat
 """
 
+# Load libraries
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+#loading dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+#checking dataset dimensions
+#shape
+print(dataset.shape)
+
+#visually see
+print(dataset.head(20))
+
+#describing dataset
+print(dataset.describe())
+
+# class distribution
+print(dataset.groupby('class').size())
+
+
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+
+# histograms
+dataset.hist()
+plt.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
